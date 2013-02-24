648a936d634b6cbcb48d4fa478c82531f0d9a97c
Hector Correa <hector@hectorcorrea.com>
2013-02-24 13:36:36 -0500

    Added find by id example

--- demoFind.js
+++ demoFind.js
@@ -5,21 +5,35 @@ db.connect('./data', function(err) {
 
   if(err) {
     console.log('Could not connect: ' + err);
+    return;
   }
-  else {
-    console.log('Connected!');
-  }
+
+  console.log('Connected!');
 
   var query = {title: 'hello world'};
   var fields = {_id: 1, title: 1};
   db.find(query, fields, function(err, documents) {
     if(err) {
       console.log('ERROR: ' + err);
+      return;
     }
-    else {
-      console.log('Found %s documents', documents.length);
-      console.dir(documents);
+
+    console.log('Found %s documents', documents.length);
+    console.dir(documents);
+  });
+
+  var idToFind = 2;
+  db.findById(idToFind, function(err, document) {
+    if (err) {
+      console.log('ERROR: ' + err);
+      return;
     }
+    if (document == null) {
+      console.log('Document %s was not found', idToFind);
+      return;
+    }
+    console.log('Found document id %s', idToFind);
+    console.dir(document);
   });
 
 });
