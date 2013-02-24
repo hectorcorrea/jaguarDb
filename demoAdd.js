a5bd4c12837bbd876f636c2a98f065415f357511
Hector Correa <hector@hectorcorrea.com>
2013-02-24 13:37:01 -0500

    minor tweaks

--- demoAdd.js
+++ demoAdd.js
@@ -1,36 +1,41 @@
 var jaguarDb = require('./jaguarDb').jaguarDb;
 var db = new jaguarDb();
 
-db.connect('./data3', function(err) {
+db.connect('./data', function(err) {
 
 	if(err) {
 		console.log('Could not connect: ' + err);
 		return;
 	}
-	else {
-		console.log('Connected!');
-	}
+
+	console.log('Connected!');
 
 	var data = {title: 'hello', content: 'blah blah blah'};
 	db.insert(data, function(err, insertedData) {
+
 		if(err) {
 			console.log('ERROR: ' + err);
+			return;
 		}
-		else {
-			console.log('Inserted: %s', JSON.stringify(insertedData));
-			updatedData = insertedData;
-			updatedData.title = 'hello world';
-			updatedData.content = 'blah-blah-blah-blah';
-			updatedData.insertedOn = new Date();
-			db.update(updatedData, function(err) {
-				if(err) {
-					console.log('ERROR: ' + err);
-				}
-				else {
-					console.log('Updated: %s', JSON.stringify(updatedData));
-				}
-			});
-		}
+
+		console.log('Inserted');
+		console.dir(insertedData);
+
+		updatedData = insertedData;
+		updatedData.title = 'hello world';
+		updatedData.content = 'blah-blah-blah-blah';
+		updatedData.insertedOn = new Date();
+		db.update(updatedData, function(err) {
+		
+			if(err) {
+				console.log('ERROR: ' + err);
+				return;
+			}
+
+			console.log('Updated');
+			console.dir(updatedData);
+		});
+
 	});
 
 });
